9551ecaa639807c6b8f0b0921368e81874d22da0
Mack Nelson <58001706+mnelson-sg@users.noreply.github.com>
2025-07-09 16:15:27 -0400

    build changes

--- index.js
+++ index.js
@@ -1,1 +1,243 @@
-(function(){"use strict";function o(e){const t=/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/g,i=new RegExp(t);return!e||e.match(i)}function l(e,t){const i={youtube:/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/,vimeo:/vimeo\.com\/([0-9]+)/,flickr:/^.*(flickr\.com)\/(.*)/,soundcloud:/^.*(soundcloud\.com|snd\.sc)\/(.*)/,twitter:/^.*(twitter\.com)\/(.*)/,instagram:/^.*(instagram\.com)\/(.*)/};return Object.keys(i).indexOf(t)==-1?!1:e.match(i[t])}var b=function(){var e=this,t=e.$createElement,i=e._self._c||t;return i("k-field",e._b({staticClass:"k-embed-field k-url-field k-field",attrs:{input:e._uid}},"k-field",e.$props,!1),[e.hasMedia?i("div",{staticClass:"preview",attrs:{"data-provider":e.providerName}},[i("div",{staticClass:"preview-content",domProps:{innerHTML:e._s(e.media.code)}}),i("div",{staticClass:"preview-background"})]):e._e(),i("k-input",e._g(e._b({ref:"input",attrs:{id:e._uid,value:e.inputValue,media:e.media,theme:"field"},on:{setMedia:e.setMedia,startLoading:e.startLoading}},"k-input",e.$props,!1),e.$listeners),[i("div",{staticClass:"k-embed-infos",attrs:{slot:"icon"},slot:"icon"},[i("div",{staticClass:"k-embed-status"},[e.loading?i("span",{staticClass:"k-embed-status-loading"},[i("span",{staticClass:"loader"})]):e.hasMedia?i("span",{staticClass:"k-embed-status-synced"},[e._v(e._s(e.$t("embed.synced"))+" "),i("span",{staticClass:"checkmark"})]):e.syncFailed?i("span",{staticClass:"k-embed-status-failed"},[e._v(e._s(e.$t("embed.failed"))+" "),i("span",{staticClass:"cross"})]):e._e()]),e.link?i("k-button",{staticClass:"k-input-icon-button",attrs:{icon:e.icon,link:e.inputValue,tooltip:e.$t("open"),tabindex:"-1",target:"_blank",rel:"noopener"}}):e._e()],1)])],1)},g=[],H="";function d(e,t,i,h,r,p,f,R){var s=typeof e=="function"?e.options:e;t&&(s.render=t,s.staticRenderFns=i,s._compiled=!0),h&&(s.functional=!0),p&&(s._scopeId="data-v-"+p);var a;if(f?(a=function(n){n=n||this.$vnode&&this.$vnode.ssrContext||this.parent&&this.parent.$vnode&&this.parent.$vnode.ssrContext,!n&&typeof __VUE_SSR_CONTEXT__!="undefined"&&(n=__VUE_SSR_CONTEXT__),r&&r.call(this,n),n&&n._registeredComponents&&n._registeredComponents.add(f)},s._ssrRegister=a):r&&(a=R?function(){r.call(this,(s.functional?this.parent:this).$root.$options.shadowRoot)}:r),a)if(s.functional){s._injectStyles=a;var U=s.render;s.render=function(x,v){return a.call(v),U(x,v)}}else{var _=s.beforeCreate;s.beforeCreate=_?[].concat(_,a):[a]}return{exports:e,options:s}}const w={extends:"k-url-field",data(){return{media:Object,loading:!1}},props:{provider:String},created(){this.value&&this.value.media&&this.hasLength(this.value.media)&&(this.media=this.value.media)},watch:{inputValue(){this.value&&this.value.media&&this.hasLength(this.value.media)?this.media=this.value.media:this.media={}}},computed:{hasMedia(){return this.hasLength(this.media)&&this.media.code},providerName(){return this.hasMedia&&this.media.providerName?this.media.providerName.toLowerCase():null},syncFailed(){return this.inputValue!=""&&this.isEmbeddableUrl(this.inputValue)&&!this.hasMedia},inputValue(){return this.value&&this.value.input?this.value.input:""}},methods:{setMedia(e){this.media=e,this.stopLoading()},hasLength(e){return Object.keys(e).length},startLoading(){this.loading=!0},stopLoading(){this.loading=!1},isEmbeddableUrl(e){return!(!o(e)||this.provider&&!l(e,this.provider))}}},u={};var k=d(w,b,g,!1,C,null,null,null);function C(e){for(let t in u)this[t]=u[t]}var $=function(){return k.exports}();const y={extends:"k-url-input",props:{provider:String,media:Object},mounted(){this.loadEmbedScripts()},methods:{onInput(e){if(e==""||!this.isEmbeddableUrl(e))return this.media={},this.emitInput(e),!1;e.includes("https://www.instagram.com")&&(e=e.split("?")[0].replace(/\/$/,"")),this.$emit("startLoading"),this.$api.get("kirby-embed/get-data",{url:e}).then(t=>{if(t.status=="success"&&t.data){if(t.data.providerName=="Vimeo"){let i=t.data.code;i=this.htmlToElement(i),i.setAttribute("referrerpolicy","strict-origin-when-cross-origin"),t.data.code=i.outerHTML}this.media=t.data}else this.media={};this.emitInput(e)}).catch(t=>{this.media={},this.emitInput(e)})},emitInput(e){this.$emit("input",{input:e,media:this.media}),this.$emit("setMedia",this.media),this.loadEmbedScripts()},loadEmbedScripts(){if(window.twttr)window.twttr.widgets.load();else if(this.media&&Object.keys(this.media).length&&this.media.providerName.toLowerCase()=="twitter"){const e=document.createElement("script");e.src="https://platform.twitter.com/widgets.js",document.body.appendChild(e)}if(window.instgrm)window.instgrm.Embeds.process();else if(this.media&&Object.keys(this.media).length&&this.media.providerName.toLowerCase()=="instagram"){const e=document.createElement("script");e.src="https://www.instagram.com/embed.js",document.body.appendChild(e)}},isEmbeddableUrl(e){return!(!o(e)||this.provider&&!l(e,this.provider))},htmlToElement(e){let t=document.createElement("template");return e=e.trim(),t.innerHTML=e,t.content.firstChild}}};let E,L;const c={};var S=d(y,E,L,!1,M,null,null,null);function M(e){for(let t in c)this[t]=c[t]}var j=function(){return S.exports}(),O=function(){var e=this,t=e.$createElement,i=e._self._c||t;return i("div",{staticClass:"k-embed-field-preview"},[i("div",{staticClass:"k-embed-field-preview-inner k-bubble",attrs:{"data-has-text":"true"}},[i("div",{staticClass:"k-embed-icon k-frame"},[i("img",{attrs:{src:e.iconSrc}})]),i("div",{staticClass:"k-embed-url"},[e._v(" "+e._s(e.url)+" ")])])])},T=[];const N={props:{value:String,field:Object},computed:{url(){return this.value.input.replace(/^\/\/|^.*?:\/\//,"")},isSynced(){return Object.keys(this.value.media).length&&this.value.media.code!==null},iconSrc(){return this.$panel.$urls.site+"/media/plugins/sylvainjule/embed/svg/"+this.providerIcon},providerIcon(){if(this.isSynced)if(this.field.icons){let e=this.value.media.providerName;return["Vimeo","Flickr","Instagram","SoundCloud","Spotify","Twitter","YouTube"].includes(e)?"embed-icon-"+e.toLowerCase()+".svg":"embed-icon-synced.svg"}else return"embed-icon-synced.svg";else return"embed-icon-failed.svg"}}},m={};var V=d(N,O,T,!1,F,null,null,null);function F(e){for(let t in m)this[t]=m[t]}var I=function(){return V.exports}();panel.plugin("sylvainjule/embed",{fields:{embed:$},components:{"k-embed-input":j,"k-embed-field-preview":I}})})();
+(function() {
+  "use strict";
+  function isUrl(value) {
+    const pattern = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/g;
+    const regex = new RegExp(pattern);
+    return !value || value.match(regex);
+  }
+  function matchProvider(value, provider) {
+    const patterns = {
+      "youtube": /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/,
+      "vimeo": /vimeo\.com\/([0-9]+)/,
+      "flickr": /^.*(flickr\.com)\/(.*)/,
+      "soundcloud": /^.*(soundcloud\.com|snd\.sc)\/(.*)/,
+      "twitter": /^.*(twitter\.com)\/(.*)/,
+      "instagram": /^.*(instagram\.com)\/(.*)/
+    };
+    const patternKeys = Object.keys(patterns);
+    if (patternKeys.indexOf(provider) == -1) return false;
+    return value.match(patterns[provider]);
+  }
+  function normalizeComponent(scriptExports, render, staticRenderFns, functionalTemplate, injectStyles, scopeId, moduleIdentifier, shadowMode) {
+    var options = typeof scriptExports === "function" ? scriptExports.options : scriptExports;
+    if (render) {
+      options.render = render;
+      options.staticRenderFns = staticRenderFns;
+      options._compiled = true;
+    }
+    return {
+      exports: scriptExports,
+      options
+    };
+  }
+  const _sfc_main$2 = {
+    extends: "k-url-field",
+    data() {
+      return {
+        media: Object,
+        loading: false
+      };
+    },
+    props: {
+      provider: String
+    },
+    created() {
+      if (this.value && this.value.media && this.hasLength(this.value.media)) {
+        this.media = this.value.media;
+        console.log(this.media);
+      }
+    },
+    watch: {
+      inputValue() {
+        if (this.value && this.value.media && this.hasLength(this.value.media)) {
+          this.media = this.value.media;
+        } else {
+          this.media = {};
+        }
+      }
+    },
+    computed: {
+      hasMedia() {
+        return this.hasLength(this.media) && this.media.code;
+      },
+      providerName() {
+        return this.hasMedia && this.media.providerName ? this.media.providerName.toLowerCase() : null;
+      },
+      syncFailed() {
+        return this.inputValue != "" && this.isEmbeddableUrl(this.inputValue) && !this.hasMedia;
+      },
+      inputValue() {
+        return this.value && this.value.input ? this.value.input : "";
+      }
+    },
+    methods: {
+      onInputValue(value) {
+        if (value === "" || !this.isEmbeddableUrl(value)) {
+          this.media = {};
+          this.emitInput(value);
+          return false;
+        }
+        if (value.includes("https://www.instagram.com")) {
+          value = value.split("?")[0].replace(/\/$/, "");
+        }
+        if (value.includes("x.com")) {
+          value = value.replace("x.com", "twitter.com");
+        }
+        this.$emit("startLoading");
+        this.$panel.api.get("kirby-embed/get-data", { url: value }).then((response) => {
+          if (response["status"] == "success" && response["data"]) {
+            if (response["data"]["providerName"] == "Vimeo") {
+              let iframe = response["data"]["code"];
+              iframe = this.htmlToElement(iframe);
+              iframe.setAttribute("referrerpolicy", "strict-origin-when-cross-origin");
+              response["data"]["code"] = iframe.outerHTML;
+            }
+            this.media = response["data"];
+          } else {
+            this.media = {};
+          }
+          this.emitInput(value);
+        }).catch((error) => {
+          this.media = {};
+          this.emitInput(value);
+        });
+      },
+      emitInput(value) {
+        this.$emit("input", { input: value, media: this.media });
+        this.$emit("setMedia", this.media);
+        this.loadEmbedScripts();
+      },
+      loadEmbedScripts() {
+        if (window.twttr) {
+          window.twttr.widgets.load();
+        } else if (this.media && Object.keys(this.media).length && this.media.providerName.toLowerCase() == "twitter") {
+          const embed = document.createElement("script");
+          embed.src = "https://platform.twitter.com/widgets.js";
+          document.body.appendChild(embed);
+        }
+        if (window.instgrm) {
+          window.instgrm.Embeds.process();
+        } else if (this.media && Object.keys(this.media).length && this.media.providerName.toLowerCase() == "instagram") {
+          const embed = document.createElement("script");
+          embed.src = "https://www.instagram.com/embed.js";
+          document.body.appendChild(embed);
+        }
+      },
+      isEmbeddableUrl(value) {
+        if (!isUrl(value)) return false;
+        if (this.provider && !matchProvider(value, this.provider)) return false;
+        return true;
+      },
+      htmlToElement(html) {
+        let template = document.createElement("template");
+        html = html.trim();
+        template.innerHTML = html;
+        return template.content.firstChild;
+      },
+      setMedia(media) {
+        this.media = media;
+        this.stopLoading();
+      },
+      hasLength(obj) {
+        return Object.keys(obj).length;
+      },
+      startLoading() {
+        this.loading = true;
+      },
+      stopLoading() {
+        this.loading = false;
+      }
+    }
+  };
+  var _sfc_render$2 = function render() {
+    var _vm = this, _c = _vm._self._c;
+    return _c("k-field", _vm._b({ staticClass: "k-embed-field k-url-field k-field", attrs: { "input": _vm._uid } }, "k-field", _vm.$props, false), [_vm.hasMedia ? _c("div", { staticClass: "preview", attrs: { "data-provider": _vm.providerName } }, [_c("div", { staticClass: "preview-content", domProps: { "innerHTML": _vm._s(_vm.media.code) } }), _c("div", { staticClass: "preview-background" })]) : _vm._e(), _c("k-input", _vm._g(_vm._b({ ref: "input", attrs: { "id": _vm._uid, "media": _vm.media.url, "theme": "field" }, on: { "input": _vm.onInputValue, "setMedia": _vm.setMedia, "startLoading": _vm.startLoading }, model: { value: _vm.inputValue, callback: function($$v) {
+      _vm.inputValue = $$v;
+    }, expression: "inputValue" } }, "k-input", _vm.$props, false), _vm.$listeners), [_c("div", { staticClass: "k-embed-infos", attrs: { "slot": "icon" }, slot: "icon" }, [_c("div", { staticClass: "k-embed-status" }, [_vm.loading ? _c("span", { staticClass: "k-embed-status-loading" }, [_c("span", { staticClass: "loader" })]) : _vm.hasMedia ? _c("span", { staticClass: "k-embed-status-synced" }, [_vm._v(_vm._s(_vm.$t("embed.synced")) + " "), _c("span", { staticClass: "checkmark" })]) : _vm.syncFailed ? _c("span", { staticClass: "k-embed-status-failed" }, [_vm._v(_vm._s(_vm.$t("embed.failed")) + " "), _c("span", { staticClass: "cross" })]) : _vm._e()]), _vm.link ? _c("k-button", { staticClass: "k-input-icon-button", attrs: { "icon": _vm.icon, "link": _vm.inputValue, "tooltip": _vm.$t("open"), "tabindex": "-1", "target": "_blank", "rel": "noopener" } }) : _vm._e()], 1)])], 1);
+  };
+  var _sfc_staticRenderFns$2 = [];
+  _sfc_render$2._withStripped = true;
+  var __component__$2 = /* @__PURE__ */ normalizeComponent(
+    _sfc_main$2,
+    _sfc_render$2,
+    _sfc_staticRenderFns$2
+  );
+  __component__$2.options.__file = "/home/mnelson/repos/sg-kirby-embed/src/components/embedField.vue";
+  const embedField = __component__$2.exports;
+  const _sfc_main$1 = {
+    extends: "k-url-input",
+    props: {
+      provider: String,
+      media: Object
+    },
+    mounted() {
+      this.loadEmbedScripts();
+    },
+    methods: {}
+  };
+  const _sfc_render$1 = null;
+  const _sfc_staticRenderFns$1 = null;
+  var __component__$1 = /* @__PURE__ */ normalizeComponent(
+    _sfc_main$1,
+    _sfc_render$1,
+    _sfc_staticRenderFns$1
+  );
+  __component__$1.options.__file = "/home/mnelson/repos/sg-kirby-embed/src/components/embedInput.vue";
+  const embedInput = __component__$1.exports;
+  const _sfc_main = {
+    props: {
+      value: String,
+      field: Object
+    },
+    computed: {
+      url() {
+        return this.value.input.replace(/^\/\/|^.*?:\/\//, "");
+      },
+      isSynced() {
+        return Object.keys(this.value.media).length && this.value.media.code !== null;
+      },
+      iconSrc() {
+        return this.$panel.$urls.site + "/media/plugins/sylvainjule/embed/svg/" + this.providerIcon;
+      },
+      providerIcon() {
+        if (this.isSynced) {
+          if (this.field.icons) {
+            let provider = this.value.media.providerName;
+            let icons = ["Vimeo", "Flickr", "Instagram", "SoundCloud", "Spotify", "Twitter", "YouTube"];
+            if (icons.includes(provider)) {
+              return "embed-icon-" + provider.toLowerCase() + ".svg";
+            } else {
+              return "embed-icon-synced.svg";
+            }
+          } else {
+            return "embed-icon-synced.svg";
+          }
+        } else {
+          return "embed-icon-failed.svg";
+        }
+      }
+    }
+  };
+  var _sfc_render = function render() {
+    var _vm = this, _c = _vm._self._c;
+    return _c("div", { staticClass: "k-embed-field-preview" }, [_c("div", { staticClass: "k-embed-field-preview-inner k-bubble", attrs: { "data-has-text": "true" } }, [_c("div", { staticClass: "k-embed-icon k-frame" }, [_c("img", { attrs: { "src": _vm.iconSrc } })]), _c("div", { staticClass: "k-embed-url" }, [_vm._v(" " + _vm._s(_vm.url) + " ")])])]);
+  };
+  var _sfc_staticRenderFns = [];
+  _sfc_render._withStripped = true;
+  var __component__ = /* @__PURE__ */ normalizeComponent(
+    _sfc_main,
+    _sfc_render,
+    _sfc_staticRenderFns
+  );
+  __component__.options.__file = "/home/mnelson/repos/sg-kirby-embed/src/components/embedPreview.vue";
+  const embedPreview = __component__.exports;
+  panel.plugin("sylvainjule/embed", {
+    fields: {
+      embed: embedField
+    },
+    components: {
+      "k-embed-input": embedInput,
+      "k-embed-field-preview": embedPreview
+    }
+  });
+})();
