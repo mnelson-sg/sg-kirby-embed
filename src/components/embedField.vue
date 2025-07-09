099bca99711e43007d7ec0115621122887013a90
Mack Nelson <58001706+mnelson-sg@users.noreply.github.com>
2025-07-09 16:12:14 -0400

    fix for kirby 5

--- src/components/embedField.vue
+++ src/components/embedField.vue
@@ -6,7 +6,7 @@
             <div class="preview-background"></div>
         </div>
 
-        <k-input ref="input" :id="_uid" v-bind="$props" :value="inputValue" :media="media" theme="field" v-on="$listeners" @setMedia="setMedia" @startLoading="startLoading">
+        <k-input ref="input" :id="_uid" v-bind="$props" @input="onInputValue" v-model="inputValue" :media="media.url" theme="field" v-on="$listeners" @setMedia="setMedia" @startLoading="startLoading">
             <div class="k-embed-infos" slot="icon">
                 <div class="k-embed-status">
                     <span v-if="loading" class="k-embed-status-loading"><span class="loader"></span></span>
@@ -25,7 +25,7 @@
 
         </k-input>
 
-      </k-field>
+    </k-field>
 </template>
 
 <script>
@@ -41,10 +41,12 @@ export default {
     },
     props: {
         provider: String,
+
     },
     created() {
         if(this.value && this.value.media && this.hasLength(this.value.media)) {
-            this.media = this.value.media
+            this.media = this.value.media;
+            console.log(this.media);
         }
     },
     watch: {
@@ -72,6 +74,77 @@ export default {
         }
     },
     methods: {
+        onInputValue(value) {
+            if(value === '' || !this.isEmbeddableUrl(value)) {
+                this.media = {}
+                this.emitInput(value)
+                return false;
+            }
+            if(value.includes('https://www.instagram.com')) {
+                value = value.split('?')[0].replace(/\/$/, "");
+            }
+            if (value.includes('x.com')) {
+                value = value.replace('x.com', 'twitter.com');
+            }
+
+            this.$emit('startLoading')
+            this.$panel.api
+                .get('kirby-embed/get-data', { url: value })
+                .then(response => {
+                    if(response['status'] == 'success' && response['data']) {
+                        if(response['data']['providerName'] == 'Vimeo') {
+                            let iframe = response['data']['code']
+                            iframe = this.htmlToElement(iframe)
+                            iframe.setAttribute('referrerpolicy', 'strict-origin-when-cross-origin')
+
+                            response['data']['code'] = iframe.outerHTML
+                        }
+                        this.media = response['data']
+                    } else {
+                        this.media = {}
+                    }
+
+                    this.emitInput(value)
+                })
+                .catch(error => {
+                    this.media = {}
+                    this.emitInput(value)
+                })
+        },
+        emitInput(value) {
+            this.$emit("input", {input: value, media: this.media});
+            this.$emit("setMedia", this.media)
+            this.loadEmbedScripts()
+        },
+        loadEmbedScripts() {
+            if (window.twttr) {
+                window.twttr.widgets.load();
+            } else if (this.media && Object.keys(this.media).length && this.media.providerName.toLowerCase() == 'twitter') {
+                const embed = document.createElement('script');
+                embed.src = 'https://platform.twitter.com/widgets.js';
+                document.body.appendChild(embed);
+            }
+
+            if (window.instgrm) {
+                window.instgrm.Embeds.process();
+            } else if (this.media && Object.keys(this.media).length && this.media.providerName.toLowerCase() == 'instagram') {
+                const embed = document.createElement('script');
+                embed.src = 'https://www.instagram.com/embed.js';
+                document.body.appendChild(embed);
+            }
+        },
+        isEmbeddableUrl(value) {
+            if (!isUrl(value)) return false
+            if (this.provider && !matchProvider(value, this.provider)) return false
+            return true
+        },
+        htmlToElement(html) {
+            let template = document.createElement('template')
+            html = html.trim()
+
+            template.innerHTML = html
+            return template.content.firstChild
+        },
         setMedia(media) {
             this.media = media
             this.stopLoading()
@@ -85,15 +158,9 @@ export default {
         stopLoading() {
             this.loading = false
         },
-        isEmbeddableUrl(value) {
-            if(!isUrl(value)) return false
-            if(this.provider && !matchProvider(value, this.provider)) return false
-            return true
-        }
     }
 };
 </script>
-
 <style lang="scss">
-    @import '../assets/css/styles.scss'
+@import '../assets/css/styles.scss'
 </style>
